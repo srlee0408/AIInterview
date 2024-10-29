d6073ae8d7279810b1bee448791d9b6990e167a2
SRLee <srlee@MacBook-Pro.local>
2024-10-29 09:28:21 +0900

    firebase hosting

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD-Cc2GRbMSNJuFdTMwBio3p8QwdbGaDgg",
+  authDomain: "aispeak-f73a6.firebaseapp.com",
+  projectId: "aispeak-f73a6",
+  storageBucket: "aispeak-f73a6.appspot.com",
+  messagingSenderId: "666072176448",
+  appId: "1:666072176448:web:f64975a7a387e19a6ecca5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import './firebase'; // Firebase 초기화 import
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
